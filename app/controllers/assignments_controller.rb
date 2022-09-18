4cfa8934fa56c0977590dafa99cacd7e0f6049e0
Manoel Bertin <manoeltirj@gmail.com>
2022-09-18 15:21:42 -0300

    color

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -25,7 +25,8 @@ class AssignmentsController < ApplicationController
 
     respond_to do |format|
       if @assignment.save
-        format.html { redirect_to assignment_url(@assignment), notice: "Assignment was successfully created." }
+        format.html { redirect_to assignments_url }
+        # format.html { redirect_to assignment_url(@assignment), notice: "Assignment was successfully created." }
         format.json { render :show, status: :created, location: @assignment }
       else
         format.html { render :new, status: :unprocessable_entity }
